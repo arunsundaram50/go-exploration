e6818dff76afaa13e24e0e8b6993717cb77a12da
Arun Sundaram <arun.sundaram@redhorsecorp.com>
2024-12-12 12:26:18 -0500

    updates

--- 7-impl-merge/1-simple-merge/merge.go
+++ 7-impl-merge/1-simple-merge/merge.go
@@ -3,15 +3,52 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+
+	"github.com/jszwec/csvutil"
 )
+
+type ItemRow struct {
+	ItemName  string
+	Quantity  int
+	IsNeeded  string
+	AddedDate string
+}
 
 func main() {
 	lhsFilename := ""
 	rhsFilename := ""
 
-	flag.StringVar(&lhsFilename, "lhs", "", "CSV filename on the left")
 	flag.StringVar(&rhsFilename, "rhs", "", "CSV filename on the right")
+	flag.StringVar(&lhsFilename, "lhs", "", "CSV filename on the left")
 	flag.Parse()
 
 	fmt.Printf("LHS=%s, RHS=%s\n", lhsFilename, rhsFilename)
+
+	lhsData, err := os.ReadFile(lhsFilename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", string(lhsData))
+
+	rhsData, err := os.ReadFile(rhsFilename)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	fmt.Printf("%s\n", string(rhsData))
+
+	// Unmarshal == Put it back in my program structure
+	var rows []ItemRow
+	fmt.Printf("Rows = %d\n", len(rows))
+	err = csvutil.Unmarshal(lhsData, &rows)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	fmt.Printf("Rows = %d\n", len(rows))
+	fmt.Printf("3rd row %v\n", rows[2])
+	fmt.Printf("All rows %v\n", rows)
 }
