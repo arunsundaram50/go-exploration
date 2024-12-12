180d797998aa8c173d931c4fc3bcf458fc18118d
Arun Sundaram <arun.sundaram@redhorsecorp.com>
2024-12-12 14:27:09 -0500

    comment updates

--- 8-pointer/1-int/main.go
+++ 8-pointer/1-int/main.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 )
 
-func f(a int) { // take a int
+func f(a int) { // takes a int value, local to this function
 	a = 100
 }
 
-func g(a *int) { // take an int address
-	*a = 100 // *a means, the value in address a
+func g(a *int) { // takes an address where int is stored
+	*a = 100 // *a means write the value 100 at the address pointed to by a (100,000,000)
 }
 
 func main() {
 
-	x := 10 // RAM location = 100,000,000 for example; value is 10
+	x := 10 // Say RAM location = 100,000,000
 	fmt.Printf("%d\n", x)
 
-	f(x) // 10
+	f(x) // 10 is passed. i.e. a copy of the value stored in x is passed
 	fmt.Printf("%d\n", x)
 
-	g(&x) // 100,000,000
+	g(&x) // 100,000,000 is passed: i.e. a copy of the address where x is stored is passed
 	fmt.Printf("%d\n", x)
 }
--- 8-pointer/2-string/main.go
+++ 8-pointer/2-string/main.go
@@ -4,23 +4,23 @@ import (
 	"fmt"
 )
 
-func f(a string) { // take a string
+func f(a string) { // takes a copy of a string
 	a = "I am f()"
 }
 
-func g(a *string) { // take an string address
+func g(a *string) { // takes an address where string is stored
 	*a = "I am g()" // *a means, the value in address a
 	fmt.Printf("address of a is %x\n", a)
 }
 
 func main() {
 
-	x := "I am main()" // RAM location = 100,000,000 for example; value is "I am main"
+	x := "I am main()" // say RAM location = 100,000,000 for example; value is "I am main"
 	fmt.Printf("%s\n", x)
 
-	f(x) // "I am main()"
+	f(x) // A copy of the value "I am main()" is passed
 	fmt.Printf("%s\n", x)
 
-	g(&x) // 100,000,000
+	g(&x) // A copy of the address, 100,000,000 is passed
 	fmt.Printf("%s\n", x)
 }
